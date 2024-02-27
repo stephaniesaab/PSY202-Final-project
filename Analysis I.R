5198ff5b4aac33b1b6ea3377d8d105bc6babebb4
Stephanie Saab <stephanie.saab@mail.utoronto.ca>
2024-02-27 11:02:59 -0500

    Starting anova

--- Analysis I.R
+++ Analysis I.R
@@ -38,7 +38,69 @@
 #======================= Packages ==============================================
 library(dplyr)
 library(tidyverse)
+library(tidyr)
+library(ggplot2)
 
 data_set <- read.csv("finalDataSet (2).csv")
 
 #================= Descriptive stats ===========================================
+
+#Remove NA entries
+data_set <- na.omit(data_set)
+
+#Mean and median for continuous variables (age, math, career, english)
+data_cont <- select(data_set, c('Age', 'English', 'Math', 'Career'))
+means <- as.data.frame(sapply(data_cont, mean))
+medians <- as.data.frame(sapply(data_cont, median))
+ranges <- sapply(data_cont, range)
+sd <- as.data.frame(sapply(data_cont, sd))
+desc_cont <- data.frame( 
+                                mean = means, 
+                                median = medians,
+                                range = ranges, 
+                                sdev = sd
+)
+
+#Frequency counts and mode for categorical variables (Intl, Eng_cat, Math_cat, Car_cat)
+data_cat <- select(data_set, c('Intl', 'Eng_cat', 'Math_cat', 'Car_cat'))
+freq_intl <- data_cat %>% count(Intl)
+freq_eng <- count(data_cat, Eng_cat)
+freq_math <- count(data_cat, Math_cat)
+freq_car <- count(data_cat, Car_cat)
+
+#============================== Correlation analysis ===========================
+#1: Math and Cognitive
+#2: English and Value
+#3: 2 DVs (difficulty and value)
+# IVs (background and demographics): age, International, math, career, english, eng_cat, math_cat, car_cat
+#DVs: affect, cognitive, value, difficulty
+# - affect = positive and negative feelings concerning stats
+# - cognitive = competence-attitudes about intellectual knowledge and skills when applied to stats
+# - value - attitudes about the usefulness, relevance, and worth of statistics
+# - difficulty - attitudes about the difficulty of statistics as a subject
+
+dat_cor <- select(data_set, c('Math', 'English', 'Affect', 'Cognitive', 'Difficulty', 'Value'))
+
+corr_1_dat <- select(data_set, 'Math', 'Cognitive')
+corr_1 <- cor(corr_1_dat, method = 'pearson')
+
+corr_2_dat <- select(data_set, 'English', 'Value')
+corr_2 <- cor(corr_2_dat, method = 'pearson')
+
+corr_3_dat <- select(data_set, 'Difficulty', 'Value')
+corr_3 <- cor(corr_3_dat, method = 'pearson')
+
+corr_total <- cor(dat_cor, method = 'pearson')
+#============================ Graphs ===========================================
+
+heatmap(corr_total, 
+        col = colorRampPalette(c('blue', 'white', 'red'))(100),
+        symm = TRUE,
+        scale = 'none', 
+        main = 'Correlation Heatmap', 
+        xlab = "Variables", 
+        ylab = 'Variables')
+#============================== Between-Subject ANOVA ==========================
+
+
+
